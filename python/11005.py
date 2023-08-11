7ce57d356a4acc12463f29dfb1cea210dbe2b6e5
eyeonze <leeyj1007987@g.skku.edu>
2023-08-11 17:58:26 +0900

    baekJoon

---
+++ python/11005.py
@@ -0,0 +1,30 @@
+# 진법 변환 2 
+# 단계 별로 풀어보기 -일반 수학 1
+# 11005 번
+# Bronze 1
+
+line = input().split(' ')
+decimal = int(line[0])
+n = int(line[1])
+
+#list 초기화
+ans = []
+
+while decimal > 0 :
+    if decimal%n < 10 :
+        # chr -> 아스키코드를 문자로 변환 / ord -> 문자를 아스키코드로 변환
+        ans.append(chr(decimal%n + 48)) 
+    else :
+        ans.append(chr(decimal%n + 55))
+    
+    decimal = int(decimal / n)
+
+ans.reverse()
+
+# list를 문자열로 변환할 때
+print(''.join(ans))
+
+
+
+
+
